7de7c92d014521bc0fd916ca37db3308ce4ff23a
NoahM <88418672+zhudianGG@users.noreply.github.com>
2026-04-20 11:42:37 -0700

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -0,0 +1,71 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution0:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head:
+            return 
+        
+        nodes = []
+        cur = head
+        while cur:
+            nodes.append(cur)
+            cur = cur.next
+        
+        i, j = 0, len(nodes) - 1
+        while i < j:
+            nodes[i].next = nodes[j]
+            i += 1
+            if i >= j:
+                break
+            nodes[j].next = nodes[i]
+            j -= 1
+        
+        nodes[i].next = None
+
+class Solution1:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+        def rec(root: ListNode, cur: ListNode) -> ListNode:
+            if not cur:
+                return root
+            
+            root = rec(root, cur.next)
+            if not root:
+                return None
+            
+            tmp = None
+            if root == cur or root.next == cur:
+                cur.next = None
+            else:
+                tmp = root.next
+                root.next = cur
+                cur.next = tmp
+            
+            return tmp
+        head = rec(head, head.next)
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+        
